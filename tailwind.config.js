75deb5b6d50567a96dde318c0d2b5d9a65b4d17a
walidelass <walidelass@gmail.com>
2024-02-01 04:17:40 -0500

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', 'index.html', './node_modules/flowbite/**/*.js'],
+  theme: {
+    colors: {
+      "primary": {
+        50: '#81D0D5',
+        100: '#66C4CB',
+        200: '#4EB9C0',
+        300: '#38ADB6',
+        400: '#24A2AB',
+        500: '#1297A1',
+        600: '#028C96',
+        700: '#017E87',
+        800: '#017078',
+        900: '#016269',
+      },
+    },
+  },
+  plugins: [require('flowbite/plugin')],
+};
